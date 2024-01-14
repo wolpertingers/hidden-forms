272f471aabdba95bdfcec3b48cd5f40fcfab5a0f
ingokuba <ingokuba@github.com>
2024-01-14 20:48:57 +0100

    more examples

---
+++ src/main/java/org/wolpertinger/hidden/forms/model/Select.java
@@ -0,0 +1,14 @@
+package org.wolpertinger.hidden.forms.model;
+
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.vaadin.flow.component.select.data.SelectListDataView;
+
+public class Select<T> extends com.vaadin.flow.component.select.Select<T> {
+
+    @SafeVarargs
+    @JsonSetter
+    @Override
+    public final SelectListDataView<T> setItems(T... items) {
+        return super.setItems(items);
+    }
+}
